1167a962a609669da5a565735f56cc2b4f49f317
JNTV <janniklas.niebisch@gmail.com>
2020-03-30 00:29:08 +0200

    v 1.2
    Fragenimport optimiert
    Code kommentiert

--- Quiz/Leaderboard.cs
+++ Quiz/Leaderboard.cs
@@ -15,27 +15,47 @@ namespace Quiz
         //Variable zum manuellen sortieren des Leaderboards
         string[,] leaderboardGetrennt;
 
+        /// <summary>
+        /// Leaderboardobjekt wird erstellt und alle Plätze aus der dazugehörigen Datei eingelesen
+        /// </summary>
         public Leaderboard()
         {
-            leaderboard = File.ReadAllLines("Leaderboard.csv");
+            leaderboard = File.ReadAllLines("Leaderboard.csv", Encoding.Default);
         }
 
+        /// <summary>
+        /// Die Platzierung wird in ihren Einzelteilen in einem String zurückgegeben
+        /// </summary>
+        /// <param name="j">Platzierung-1</param>
+        /// <returns>String-Array mit den Details zur Platzierung (Name, Punktzahl, Thema)</returns>
         public string[] GetPlatzierung(int j)
         {
             return leaderboard[j].Split(';');
         }
+
+        /// <summary>
+        /// Größe des Leaderboards wird ermittelt
+        /// </summary>
+        /// <returns>Größe des Leaderboards</returns>
         public int GetLeaderboardGroeße()
         {
             return leaderboard.Length;
         }
 
+        /// <summary>
+        /// Leaderboard wird aktualisiert und die Datei um die neue Platzierung aktualisiert
+        /// </summary>
+        /// <param name="name">Name des Spielers</param>
+        /// <param name="punktzahl">Punktzahl des Spielers</param>
+        /// <param name="thema">Gespieltes Thema</param>
         public void LeaderboardAktualisieren(string name, int punktzahl, string thema)
         {
-
             if (leaderboard.Length > 1)
             {
+                //Array zur Aufsplittung des Leaderboards in seine Bestandteile wird erstellt
                 leaderboardGetrennt = new String[3, leaderboard.Length + 1];
 
+                //Einzelne Bestandteile des Leaderboards werden ins Array geladen
                 for (int i = 0; i < leaderboard.Length; i++)
                 {
                     string[] value = leaderboard[i].Split(';');
@@ -44,10 +64,12 @@ namespace Quiz
                     leaderboardGetrennt[2, i] = value[2];
                 }
 
+                //Der neue Spieler wird ins Leaderboard geladen
                 leaderboardGetrennt[0, leaderboard.Length] = name;
                 leaderboardGetrennt[1, leaderboard.Length] = Convert.ToString(punktzahl);
                 leaderboardGetrennt[2, leaderboard.Length] = thema;
 
+                //Das Leaderboard wird nach dem Sort-Algorithmus sortiert
                 for (int i = leaderboard.Length; i > 0; i--)
                 {
                     for (int j = 1; j <= i; j++)
@@ -66,15 +88,20 @@ namespace Quiz
                         }
                     }
                 }
+
+                //Ein neues Leaderboard-Array wird gebildet um das aktuelle und sortierte Leaderboard wieder zusammenzufügen
                 leaderboard = new string[leaderboardGetrennt.GetUpperBound(1) + 1];
+
+                //Das Leaderboard wird wieder in eine eindimensioales Array zusammengefügt
                 for (int i = 0; i < leaderboardGetrennt.GetUpperBound(1) + 1; i++)
                 {
                     leaderboard[i] = leaderboardGetrennt[0, i] + ";" + leaderboardGetrennt[1, i] + ";" + leaderboardGetrennt[2, i];
                 }
 
-
+                //Das alte Leaderboard wird gelöscht
                 File.Delete("Leaderboard.csv");
 
+                //Das neue Leaderboard wird wieder in die Datei geschrieben
                 strom = new FileStream("Leaderboard.csv", FileMode.Create, FileAccess.Write);
                 StreamWriter schreiben = new StreamWriter(strom);
                 for (int i = 0; i < leaderboard.Length; i++)
@@ -83,6 +110,8 @@ namespace Quiz
                 }
                 schreiben.Close();
             }
+
+            //Wenn es noch keinen Eintrag im Leaderboard gibt, dann wird das alte gelöscht und ein neues mit dem ersten Eintrag erstellt 
             else
             {
                 File.Delete("Leaderboard.csv");
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Coronaquiz
-Quiz für die Zeit während Corona
+Hallo zusammen. Danke, dass du mein Spiel spielst. Das Spiel ist als Schulprojekt entstanden und besteht aus 3 kleinen Quizzen.
+
+Um das Spiel zu spielen, musst du die Quiz.zip entpacken und musst dann nur noch die .exe starten. Ich wünsche dir viel Spaß beim Spielen :)
